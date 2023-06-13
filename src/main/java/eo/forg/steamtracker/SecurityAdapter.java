ca532e0f47a4de685e4d445a728c8744d7f0368c
forgjayjay <edlink2004@gmail.com>
2023-06-13 12:45:36 +0200

    Fixed spring security demanding user credentials

--- src/main/java/eo/forg/steamtracker/SecurityAdapter.java
+++ src/main/java/eo/forg/steamtracker/SecurityAdapter.java
@@ -1,6 +1,8 @@
 package eo.forg.steamtracker;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,14 +11,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityAdapter {
+  @Bean
+  @Order(Integer.MAX_VALUE-10)
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    return http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> {
-          auth.requestMatchers("/").permitAll();
-          auth.anyRequest().permitAll();
-        })
-        .httpBasic(Customizer.withDefaults())
-        .build();
+      return http
+          .csrf(csrf -> csrf.disable())
+          .authorizeHttpRequests(auth -> {
+            auth.anyRequest().permitAll();
+          })
+          .httpBasic(Customizer.withDefaults())
+          .build();    
   }
 }
